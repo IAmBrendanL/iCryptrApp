672514265797cc6eda69c2295487c4e081048d66
Brendan Lindsey <brendan_lindsey@outlook.com>
2021-08-17 18:28:42 -0700

    Initial Commit

---
+++ Shared/iCryptrApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCryptrApp.swift
+//  Shared
+//
+//  Created by Brendan Lindsey on 8/17/21.
+//
+
+import SwiftUI
+
+@main
+struct iCryptrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
