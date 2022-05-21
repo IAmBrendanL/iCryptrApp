9b963a9eb7ff6a4dd8ccc7103fa7f6b93bf45556
Brendan Lindsey <brendan_lindsey@outlook.com>
2022-05-21 13:17:00 -0700

    Added basic view

--- iCryptr/ViewController.swift
+++ iCryptr/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController  {
+    @IBOutlet weak var EncryptPhotoButton: UIButton!
+    @IBOutlet weak var EncryptFileButton: UIButton!
+    @IBOutlet weak var DecryptButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func EncryptPhotoTouchUpInside(_ sender: Any) {
+        print("Touched Encrypt Photo")
+    }
+    @IBAction func EncryptFileTouchUpInside(_ sender: Any) {
+        print("Touched Encrpyt File")
+    }
+    @IBAction func DecryptTouchUpInside(_ sender: Any) {
+        print("Touched Decrypt")
+    }
 }
 
